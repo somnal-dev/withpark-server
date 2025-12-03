00cbf7e4b7c9dfe9f433730f1ff3f9bcbc272a8c
솜놜 <90139018+somnal-dev@users.noreply.github.com>
2025-12-04 00:04:42 +0900

    feat: 네이버 로그인 구현

---
+++ src/main/kotlin/com/somnal/app/withpark/domain/oauth/dto/NaverTokenResponseDto.kt
@@ -0,0 +1,18 @@
+package com.somnal.app.withpark.domain.oauth.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class NaverTokenResponseDto(
+    @JsonProperty("access_token")
+    val accessToken: String,
+    @JsonProperty("refresh_token")
+    val refreshToken: String,
+    @JsonProperty("token_type")
+    val tokenType: String,
+    @JsonProperty("expires_in")
+    val expiresIn: Long,
+    @JsonProperty("error")
+    val error: String,
+    @JsonProperty("error_description")
+    val errorDescription: String,
+)
